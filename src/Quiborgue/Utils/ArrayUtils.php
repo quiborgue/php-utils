e169598da14f4010f36575110f18da8d5232f7bc
Carlos Augusto Borges <carlos.borges@tst.jus.br>
2014-11-18 17:27:16 -0200

    Making comparison independent of order

--- src/Quiborgue/Utils/ArrayUtils.php
+++ src/Quiborgue/Utils/ArrayUtils.php
@@ -1,7 +1,13 @@
 <?php namespace Quiborgue\Utils;
 class ArrayUtils {
 	public static function diff_assoc_recursive($array1, $array2) { 
-		foreach($array1 as $key => $value) { 
+		if (count($array2) > count($array1)) {
+			$aux = $array1;
+			$array1 = $array2;
+			$array2 = $aux;
+		}
+		
+		foreach($array1 as $key => $value) {
 			if(is_array($value)) { 
 				if(!isset($array2[$key])) { 
 					$difference[$key] = $value; 
@@ -15,7 +21,7 @@ class ArrayUtils {
 				} 
 			} elseif(!isset($array2[$key]) || $array2[$key] != $value) { 
 				$difference[$key] = $value; 
-			} 
+			}
 		} 
 		return !isset($difference) ? array() : $difference; 
 	}
